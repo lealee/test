38698e656f0423a37ee05d113c74d770a714f7a7
CTLee <cherubic102@gmail.com>
2013-05-15 16:41:02 +0800

    index

---
+++ jo.php
@@ -0,0 +1,53 @@
+<head>
+
+<style type=text/css>
+<!--
+body{
+background-image:url(https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-frc1/267695_553848007999497_582165238_n.jpg);
+background-position:80% 20%;
+background-attachment:fixed;
+background-repeat:no-repeat;}
+-->
+</style>
+<font color="#dedede" face="Verdana" size="6">
+        Runners Records
+</font><br>
+<font color="#37FDFF" face="Verdana" size="4">
+        copyright SooChow
+</font>
+</head>
+<br>
+<br>
+<br>
+<body bgcolor="black">
+<style type="text/css">
+
+input.groovybutton
+{
+   font-size:15px;
+   font-family:Verdana,sans-serif;
+   font-weight:bold;
+   color:#999999;
+   width:200px;
+   height:50px;
+   background-color:#FFFFFF;
+   border-style:solid;
+   border-color:#FFFFFF;
+   border-width:3px;
+}
+
+</style>
+
+
+<form name="groovyform">
+<input
+   type="button"
+   name="groovybtn1"
+   class="groovybutton"
+   value="Search"
+   title="">
+</form>
+<font color="white">
+查詢男女組
+</font>
+</body>
